4c56e857de6189c86fa15636ed385a139e5dc943
SebasMac <ionrodriguezlara@gmail.com>
2023-09-05 09:12:20 -0700

    First Commit

---
+++ ContactList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContactList
+//
+//  Created by Ion Sebastian Rodriguez Lara on 05/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
